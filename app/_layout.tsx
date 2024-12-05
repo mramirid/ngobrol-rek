e1b021b08fabdaf9e60528769dc88a42d201b1fa
mramirid <amir.muh.hakim@gmail.com>
2024-12-05 20:07:48 +0700

    refactor: move `AuthProvider` to screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,12 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 
-import AuthProvider from "@/components/auth/AuthProvider";
-
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <AuthProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-      </AuthProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
     </PaperProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 
 import AppBar from "@/components/AppBar";
+import AuthProvider from "@/components/auth/AuthProvider";
 import LoginPromptProvider from "@/components/login-prompt/LoginPromptProvider";
 
 export default function Index() {
   return (
-    <LoginPromptProvider>
-      <AppBar />
-      <View style={styles.screen}>
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-      </View>
-    </LoginPromptProvider>
+    <AuthProvider>
+      <LoginPromptProvider>
+        <AppBar />
+        <View style={styles.screen}>
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+      </LoginPromptProvider>
+    </AuthProvider>
   );
 }
 
