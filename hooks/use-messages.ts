ac0bea850b33c651f4588f2c86284f1e039e451a
mramirid <amir.muh.hakim@gmail.com>
2024-12-10 09:43:05 +0700

    refactor(chat): unimplement the message listener

--- hooks/use-messages.ts
+++ hooks/use-messages.ts
@@ -1,15 +1,6 @@
-import {
-  addDoc,
-  collection,
-  limit,
-  onSnapshot,
-  orderBy,
-  query,
-} from "firebase/firestore";
 import { useSyncExternalStore } from "react";
-import { GiftedChat, IMessage } from "react-native-gifted-chat";
+import { IMessage } from "react-native-gifted-chat";
 
-import { db } from "@/constants/firebase";
 import UnimplementedError from "@/errors/unimplemented.error";
 
 export default function useMessages() {
@@ -25,39 +16,9 @@ export default function useMessages() {
 
 let messages: IMessage[] = [];
 
-const MAX_LOADED_MESSAGES = 100;
-
-function subscribeMessages(notifyNewMessages: () => void) {
-  const unsubscribe = onSnapshot(
-    query(
-      collection(db, "messages"),
-      orderBy("createdAt", "desc"),
-      limit(MAX_LOADED_MESSAGES)
-    ),
-    (snapshot) => {
-      const incomingMessags = snapshot
-        .docChanges()
-        .filter((docChange) => docChange.type === "added")
-        .map<IMessage>((docChange) => {
-          const docId = docChange.doc.id;
-          const docData = docChange.doc.data();
-          return {
-            _id: docId,
-            text: docData.text,
-            createdAt: docData.createdAt.toDate(),
-            user: {
-              _id: docData.user._id,
-              name: docData.user._id,
-            },
-          };
-        });
-      messages = GiftedChat.append(messages, incomingMessags).slice(
-        0,
-        MAX_LOADED_MESSAGES
-      );
-      notifyNewMessages();
-    }
-  );
+function subscribeMessages(_notifyNewMessages: () => void) {
+  // TODO: listen to collection "messages"
+  const unsubscribe = () => {};
 
   return () => {
     messages = [];
